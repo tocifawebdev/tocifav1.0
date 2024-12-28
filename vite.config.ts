9a425676b5935b226b2d1940349e7ab8ff3dbdd5
tocifawebdev <yosephsi21@gmail.com>
2024-12-29 03:47:00 +0700

    dd

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
-      '/login': { 
+      '/loginn': { 
         target: 'http://127.0.0.1:8080',
         changeOrigin: true,
         secure: false,
