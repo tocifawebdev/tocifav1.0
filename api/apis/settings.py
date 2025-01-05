9e8eb4a314e8e11ada9b38c9b3de5bbdef91a77b
tocifawebdev <yosephsi21@gmail.com>
2025-01-06 00:06:15 +0700

    Update #3

--- api/apis/settings.py
+++ api/apis/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from decouple import config
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'.asd
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
