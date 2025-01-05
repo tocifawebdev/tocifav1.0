2ac7950b61159c03a066f7b64d6d442cc795366e
tocifawebdev <yosephsi21@gmail.com>
2025-01-06 00:06:34 +0700

    Revert "Update #3"
    This reverts commit 9e8eb4a314e8e11ada9b38c9b3de5bbdef91a77b.

--- api/apis/settings.py
+++ api/apis/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from decouple import config
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.asd
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
